18052af28b45961490bf9276db7fcea083399228
prathmeshk-k2 <prathmeshkokare009@gmail.com>
2025-12-23 19:50:52 +0530

    code changes

--- APIassignment/routers/admin.js
+++ APIassignment/routers/admin.js
@@ -6,54 +6,14 @@ const CryptoJS = require("crypto-js");
 const jwt=require("jsonwebtoken")
 const result=require("../utils/result")
 const config=require("../utils/config")
+const { checkAuthorization } = require("../utils/auth");
 
 
 const router=express.Router()
 
 
-router.post("/signup",(req,res)=>{
-    const{email,password,role}=req.body
-    const sql = `INSERT INTO users(email,password,role)VALUES(?,?,?)`
-    const hashedPassword = CryptoJS.SHA256(password).toString();
-
-    pool.query(sql,[email,hashedPassword,role],(error,data)=>{
-        res.send(result.createResult(error,data))
-    })
-
-})
-
-router.post("/login", (req, res) => {
-  const { email, password } = req.body;
- const hashedPassword = CryptoJS.SHA256(password).toString();
-  const sql = "SELECT * FROM users WHERE email = ? AND password=?";
-
-  pool.query(sql, [email, hashedPassword], (error, data) => {
-        if (error)
-            res.send(result.createResult(error))
-        else if (data.length == 0)
-            res.send(result.createResult("Invalid email or password"))
-        else {
-            const user= data[0]
-         
-            const payload={
-                email: user.email,
-                role : user.role
-            }
-            const token=jwt.sign(payload,config.SECRET)
-            const userData={
-                email : user.email,
-                role: user.role,
-                token
-            }
-           
-            res.send(result.createResult(null, userData))
-        }
-    })
-})
-
-
 //add new courses
-router.post('/addCourse', (req, res) => {
+router.post('/addCourse', checkAuthorization,(req, res) => {
   const { course_name,description,fees,start_date,end_date,video_expire_days} = req.body
 
   const sql = `INSERT INTO courses(course_name, description, fees, start_date, end_date, video_expire_days) VALUES (?, ?, ?, ?, ?, ?)`
@@ -92,7 +52,7 @@ router.get('/all-courses', (req, res) => {
 })
 
 //updatate a course by course_id
-router.put("/update/:courseId", (req, res) => {
+router.put("/update/:courseId",checkAuthorization, (req, res) => {
   const { courseId } = req.params
   const { course_name, description,fees, start_date, end_date,video_expire_days} = req.body
 
@@ -107,7 +67,7 @@ router.put("/update/:courseId", (req, res) => {
 
 
 //delete a course by course_id
-router.delete("/delete/:courseId",(req, res) => {
+router.delete("/delete/:courseId",checkAuthorization,(req, res) => {
   const { courseId } = req.params
 
   const sql = `DELETE FROM courses WHERE course_id = ?`
@@ -116,6 +76,33 @@ router.delete("/delete/:courseId",(req, res) => {
     res.send(result.createResult(error, data))
   })
 })
+
+// entrolled student based on course_id
+router.get("/enrolled-students",checkAuthorization, (req, res) => {
+  const { course_id } = req.query;
+
+  if (!course_id) {
+    return res.send(result.createResult("courseId is required", null));
+  }
+
+  const sql = `
+    SELECT 
+      s.reg_no,
+      s.name,
+      s.email,
+      s.mobile_no,
+      c.course_id,
+      c.course_name
+    FROM students s
+    JOIN courses c ON s.course_id = c.course_id
+    WHERE c.course_id = ?
+    ORDER BY s.name
+  `;
+
+  pool.query(sql, [course_id], (err, data) => {
+    res.send(result.createResult(err, data));
+  });
+});
 
 
 module.exports = router;
--- APIassignment/routers/videos.js
+++ APIassignment/routers/videos.js
@@ -1,80 +1,13 @@
-const express = require("express");
-const pool = require("../db/pool");
-const result = require("../utils/result");
-const adminAuth = require("../utils/auth");
+const express = require("express")
+const pool = require("../db/pool")
+const result = require("../utils/result")
+const { checkAuthorization } = require("../utils/auth")
+
 const router=express.Router();
 
 
-//get all active courses
-
-// router.get('/all-active-courses', (req,res) => {
-//    const sql = ` SELECT * FROM courses WHERE start_date <= CURDATE()AND end_date >= CURDATE() `
-//   pool.query(sql, (error, data) => {
-//     res.send(result.createResult(error, data))
-//    })
-// })
-
-//get all courses
-
-// router.get('/all-courses', (req, res) => {
-//   const { start_date, end_date } = req.query
-
-//   let sql = `SELECT * FROM courses`
-
-//   if (start_date && end_date) {
-//     sql += ` WHERE start_date >= '${start_date}' AND end_date <= '${end_date}'`
-//   }
-
-//   pool.query(sql, (error, data) => {
-//     res.send(result.createResult(error, data))
-//   })
-// })
-
-
-//add new courses
-// router.post('/addCourse', (req, res) => {
-//   const { course_name,description,fees,start_date,end_date,video_expire_days} = req.body
-
-//   const sql = `INSERT INTO courses(course_name, description, fees, start_date, end_date, video_expire_days) VALUES (?, ?, ?, ?, ?, ?)`
-
-//   pool.query(
-//     sql,
-//     [course_name, description, fees, start_date, end_date, video_expire_days],
-//     (error, data) => {
-//       res.send(result.createResult(error, data))
-//     }
-//   )
-// })
-
-
-//updatate a course by course_id
-// router.put("/update/:courseId", (req, res) => {
-//   const { courseId } = req.params
-//   const { course_name, description,fees, start_date, end_date,video_expire_days} = req.body
-
-//   const sql = ` UPDATE courses SET course_name = ?, description = ?, fees = ?,start_date = ?, end_date = ?, video_expire_days = ?
-//     WHERE course_id = ?`
-
-//   pool.query( sql, [course_name, description, fees, start_date, end_date, video_expire_days, courseId], (error, data) => {
-//       res.send(result.createResult(error, data))
-//     }
-//   )
-// })
-
-//delete a course by course_id
-// router.delete("/delete/:courseId", (req, res) => {
-//   const { courseId } = req.params
-
-//   const sql = `DELETE FROM courses WHERE course_id = ?`
-
-//   pool.query(sql, [courseId], (error, data) => {
-//     res.send(result.createResult(error, data))
-//   })
-// })
-
-
 //toget all videos
-router.get("/all-videos", adminAuth, (req, res) => {
+router.get("/all-videos",checkAuthorization, (req, res) => {
   const { courseId } = req.query;
 
   let sql = "SELECT * FROM videos";
@@ -91,7 +24,7 @@ router.get("/all-videos", adminAuth, (req, res) => {
 });
 
 //add video
-router.post("/add", adminAuth, (req, res) => {
+router.post("/add", checkAuthorization,(req, res) => {
   const { courseId, title,  description, youtubeURL} = req.body;
 
   const sql = `
@@ -108,7 +41,7 @@ router.post("/add", adminAuth, (req, res) => {
 
 
 //update video
-router.put("/update/:videoId", adminAuth, (req, res) => {
+router.put("/update/:videoId", checkAuthorization, (req, res) => {
   const { videoId } = req.params;
   const { courseId, title, youtubeURL, description } = req.body;
 
@@ -127,7 +60,7 @@ router.put("/update/:videoId", adminAuth, (req, res) => {
 
 
 //delete video
-router.delete("/delete/:videoId", adminAuth, (req, res) => {
+router.delete("/delete/:videoId",checkAuthorization,  (req, res) => {
   const { videoId } = req.params;
 
   pool.query(
@@ -135,79 +68,6 @@ router.delete("/delete/:videoId", adminAuth, (req, res) => {
     [videoId],
     (err, data) => res.send(result.createResult(err, data))
   );
-});
-
-///student/my-courses GET get all registered courses of a student
-router.get('/my-courses', (req, res) => {
-  const { email } = req.query;
-   const sql = `SELECT *
-    FROM students s
-    JOIN courses c ON s.course_id = c.course_id
-    WHERE s.email = ?
-  `;
-
-  pool.query(sql, [email], (error, data) => {
-    res.send(result.createResult(error, data));
-  })
-});
-
-
-//registers student to a course
-router.post('/register-to-course',(req,res)=>{
-    //console.log ("registration");
-    const {name, email, course_id, mobile_no} =req.body;
-    const sql=`insert into students (name, email, course_id, mobile_no) values(?,?,?,?)`;
-    pool.query(sql,[name, email, course_id, mobile_no],(error,data)=>{
-         res.send(result.createResult(error, data))
-    })
-    
-});
-
-// change password
-
-router.put("/change-password",(req,res) =>{
-  const{email,newPassword,confirmpassword}= req.body;
- if(newPassword != confirmpassword)
-{
-    return res.status(400).json({
-      message: "New password and confirm password do not match"
-    });
-}
-const sql = `UPDATE users SET password = ? WHERE email = ?`;
-
-
-pool.query(sql,[newPassword,email],(error,data)=>{
-     res.send(result.createResult(error, data))
-});
-});
-
-
-
-// my-course with videos
-router.get("/my-course-videos", (req, res) => {
-  const { email } = req.query;
-
-  if (!email) {
-    return res.status(400).json({ message: "email is required" });
-  }
-
-  const sql = `
-SELECT s.name, c.course_name, v.title, v.youtube_url
-FROM students s
-JOIN courses c ON s.course_id = c.course_id
-JOIN videos v ON c.course_id = v.course_id
-WHERE s.email = ?`;
-
-//student put
-//new password,change password
-router.put("/change-password", (req, res) => {
-    const { email, newPassword, confirmPassword } = req.body
-
-    if (newPassword !== confirmPassword) {
-        res.send(result.createResult("Password not matching", null))
-        return
-    }
-  });
 });
 
 
