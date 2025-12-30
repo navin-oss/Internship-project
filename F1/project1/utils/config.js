9a66313be1be0f69bb806fbc8b51e6e6dc3ce5b7
Pranoti-Sankpal <pranotisankapl76@gmail.com>
2025-12-30 09:34:27 +0530

    with hashed password

---
+++ F1/project1/utils/config.js
@@ -0,0 +1,9 @@
+const SECRET = 'kcbksdkfbwebweaibduewvufyiewbdiywefbuibdsibd'
+
+module.exports = { SECRET }
+
+module.exports = {
+  SECRET: "mysecret",
+  ADMIN_EMAIL: "admin@gmail.com",
+  ADMIN_PASSWORD: "admin123"
+};
